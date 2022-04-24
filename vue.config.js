84a33b992d8e174ff8e802e51c90f9fbdb442230
kingdom32307 <kingdom32307@gmail.com>
2022-04-24 10:45:51 +0900

    doc

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/KissShot/'   //任意
-      : '/'
+    outputDir: 'docs',
+    assetsDir: './',
+    publicPath: './'
   }
